997ef08fa64e4ad53e9cd9ad109beee731274fce
Jessie Sanchez <jessie@jsan.codes>
2022-01-09 18:06:19 -0700

    initial

---
+++ Azure-ARM-Templates/VNet/Create ResourceGroup/CreateResourceGroup.ps1
@@ -0,0 +1,86 @@
+
+<#
+ .SYNOPSIS
+    Template for creating Resource Group
+#>
+
+$subscriptionId = "46430462-954b-4968-b902-c60e2497443f" #insert your subscription ID
+$resourceGroupName = "RG-JSAN-SCUS"  #provide resource group name
+$resourceGroupLocation = "SouthCentralUS"  #location
+$tags = @{"Category"=“JSAN"; "Network"=“JSAN"; "Project"=“1"} 
+
+
+Function RegisterRP {
+    Param(
+        [string]$ResourceProviderNamespace
+    )
+
+    Write-Host "Registering resource provider '$ResourceProviderNamespace'";
+    Register-AzResourceProvider -ProviderNamespace $ResourceProviderNamespace;
+}
+
+#******************************************************************************
+# Script body
+# Execution begins here
+#******************************************************************************
+$ErrorActionPreference = "Stop"
+
+# sign in
+Connect-AzAccount
+
+# select subscription
+Write-Host "Selecting subscription '$subscriptionId'";
+Select-AzSubscription -SubscriptionID $subscriptionId;
+
+# Register RPs
+# AZURE CLI COMMAND - Get-AzResourceProvider -ListAvailable | Select-Object ProviderNamespace, RegistrationState
+$resourceProviders = @("microsoft.web");
+if($resourceProviders.length) {
+    Write-Host "**Registering resource providers"
+    foreach($resourceProvider in $resourceProviders) {
+        RegisterRP($resourceProvider);
+    }
+}
+
+#Create or check for existing resource group
+$resourceGroup = Get-AzResourceGroup -Name $resourceGroupName -ErrorAction SilentlyContinue
+if(!$resourceGroup)
+{
+    Write-Host "***Resource group '$resourceGroupName' does not exist. To create a new resource group, please enter a location.";
+    if(!$resourceGroupLocation) {
+        $resourceGroupLocation = Read-Host "resourceGroupLocation";
+    }
+    Write-Host "**Creating resource group '$resourceGroupName' in location '$resourceGroupLocation'";
+    New-AzResourceGroup -Name $resourceGroupName -Location $resourceGroupLocation -Tag $tags
+    
+}
+else{
+    Write-Host "***Using existing resource group '$resourceGroupName'";
+}
+
+
+
+
+
+
+
+# Start the deployment from Remote Template 
+Write-Host "Starting deployment from Github Repo...";
+New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateUri $templateFileURI -TemplateParameterUri $parametersFileURI -Verbose;
+
+# OR
+
+
+
+
+
+
+
+# Start the deployment from Local File
+
+Write-Host "Starting deployment from Local Repo...";
+if(Test-Path $parametersFileURI) {
+    New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $templateFilePath -TemplateParameterUri $parametersFileURI -Verbose;
+} else {
+    New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $templateFilePath -Verbose;
+}
---
+++ LearnDeploy.ps1
@@ -0,0 +1,9 @@
+
+
+az group list -o table
+
+az account set --subscription 46430462-954b-4968-b902-c60e2497443f
+
+az group create --name RG-JSAN-SCUS --location southcentralus --tags Category=JSAN Network=JSAN Project=1
+
+az group delete --name exampleGroup
